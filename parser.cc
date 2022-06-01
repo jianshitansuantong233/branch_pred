d22a4ba2fba93ddadb65c649bafe6b4b4e72af40
Yijun Li <1215664347@qq.com>
2022-06-01 04:41:28 -0700

    testing new network

--- parser.cc
+++ parser.cc
@@ -9,9 +9,9 @@ using namespace std;
 
 #include "utils.h"
 #include "bt9_reader.h"
-
+#include "Pickle-in-Cpp/pickle.h"
 #define SAVE_CSV 1
-
+// #define SAVE_PKL 1
 struct BinDataPoint {
     bool branchTaken;
     bool predDir;
@@ -30,13 +30,6 @@ int main(int argc, char *argv[]) {
     // TODO: batch process all traces
     std::string trace_path;
     trace_path = argv[1];
-
-#ifdef SAVE_CSV
-    std::ofstream csvFile;
-    csvFile.open(trace_path + ".csv");
-
-    csvFile << "PC,branchTaken,branchTarget,history1,history2,history3,nonBranchInstrCount,observedTraversCount\n";
-#endif
 
     bt9::BT9Reader bt9_reader(trace_path);
 
@@ -58,57 +51,85 @@ int main(int argc, char *argv[]) {
     UINT64 numIter = 0;
     std::map<UINT64, vector<int>> history;
 
-    for (auto it = bt9_reader.begin(); it != bt9_reader.end(); ++it) {
+#ifdef SAVE_CSV
+    std::ofstream csvFile;
+    csvFile.open("/CBP-16-Simulation/cbp2016.eval/parsed_traces/"+trace_path + std::to_string(numIter) + ".csv");
+
+    csvFile << "PC,branchTaken,branchTarget,history1,history2,history3\n";
+#endif
+
+#ifdef SAVE_PKL
+#endif
+    for (auto it = bt9_reader.begin(); it!=bt9_reader.end(); ++it) {
+        if(numIter%1024==0){
+#ifdef SAVE_CSV
+            csvFile.close();
+            csvFile.open("/CBP-16-Simulation/cbp2016.eval/parsed_traces/"+trace_path + std::to_string(int(numIter/1024)) + ".csv");
+
+            csvFile << "PC,branchTaken,branchTarget,history1,history2,history3\n";
+#endif
+        }
         PC = it->getSrcNode()->brVirtualAddr();
+        if(PC==0) continue;
         branchTarget = it->getEdge()->brVirtualTarget();
         branchTaken = it->getEdge()->isTakenPath();
-
+        numIter++;
 #ifdef SAVE_CSV
         csvFile << std::to_string(PC) + ","
             << std::to_string(branchTaken) + ","
             << std::to_string(branchTarget) + ",";
 #endif  
-
+#ifdef SAVE_PKL
+#endif
         if(history.find(PC) != history.end()){
             // Not first time to visit
             // maintain history vector
 #ifdef SAVE_CSV
-        csvFile << history[PC][0]<<","<<history[PC][1]<<","<<history[PC][2]<<",";
+        csvFile << history[PC][0]<<","<<history[PC][1]<<","<<history[PC][2]<<"\n";
 #endif 
+#ifdef SAVE_PKL
+#endif
             // too lazy to optimize
-            if(history[PC][1] == -1){
+            if(history[PC][1] == 0){
                 history[PC][1] = history[PC][0];
             }
-            else if(history[PC][2] == -1){
+            else if(history[PC][2] == 0){
                 history[PC][2] = history[PC][1];
                 history[PC][1] = history[PC][0];
             }
-            else if(history[PC][2] != -1){
+            else if(history[PC][2] != 0){
                 history[PC][2] = history[PC][1];
                 history[PC][1] = history[PC][0];
             }
 
-            history[PC][0] = branchTaken?1:0;
+            history[PC][0] = branchTaken?2:1;
         }
         else{
             // First time to see
 #ifdef SAVE_CSV
-        csvFile << "-1,-1,-1,";
+        csvFile << "0,0,0\n";
 #endif 
-            vector<int> tmp(3, -1);
-            tmp[0] = branchTaken?1:0;
+#ifdef SAVE_PKL
+#endif
+            vector<int> tmp(3, 0);
+            tmp[0] = branchTaken?2:1;
             history.insert(std::make_pair(PC, tmp));
         }
-        nbr_instr_count = it->getEdge()->nonBrInstCnt();
-        obs_trav_count = it->getEdge()->observedTraverseCnt();
+        // nbr_instr_count = it->getEdge()->nonBrInstCnt();
+        // obs_trav_count = it->getEdge()->observedTraverseCnt();
 #ifdef SAVE_CSV
-        csvFile << std::to_string(nbr_instr_count) +","
-            << std::to_string(obs_trav_count)+"\n";
+        // csvFile << "\n";
+        // csvFile << std::to_string(nbr_instr_count) +","
+            // << std::to_string(obs_trav_count)+"\n";
+#endif
+#ifdef SAVE_PKL
 #endif 
     }
 
 #ifdef SAVE_CSV
     csvFile.close();
+#endif
+#ifdef SAVE_PKL
 #endif
     return 0;
 
